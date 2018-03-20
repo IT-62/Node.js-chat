ea2bd64e5eecf749ceb66cddd165efa2be164b6c
zayetsmykyta <dazyhero@gmail.com>
2018-03-20 11:26:34 +0200

    shit code

--- client.js
+++ client.js
@@ -1,32 +1,45 @@
 'use strict';
 
 const net = require('net');
-
 const readline = require('readline');
 
-const name = 'Nikita';
+let username;
+let user;
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-const socket = new net.Socket();
-
-
-socket.connect({
-  port: 2000,
-  host: '169.254.15.163',
-},() => {
-  socket.write(JSON.stringify(name));
+const client = net.createConnection({
+  host: '127.0.0.1',
+  port: 2000
+}, () => {
+  rl.question('Set username: ', (answer) => {
+    username = answer;
+    user = {
+      type: 'setName',
+      message: username
+    };
+    client.write(JSON.stringify(user));
+    rl.pause();
+  });
 });
 
-socket.on('data', (data) => {
-  let result = JSON.parse(data);
+client.on('data', (data) => {
+  rl.resume();
+  const result = JSON.parse(data);
   console.clear();
   console.log(result);
-})
+});
+
 rl.on('line', (input) => {
-  let message = `${name} : ${input}`
-  input === 'end' ? rl.close() : socket.write(JSON.stringify(message));
+  user = {
+    name: username,
+    type: 'message',
+    message: input
+  };
+  input === 'end' ? client.destroy() :
+    client.write(JSON.stringify(user));
+  rl.pause();
 });
--- server.js
+++ server.js
@@ -1,21 +1,45 @@
-'use strict';
+//DRAFT FILE
+'use strict'
 
 const net = require('net');
 const users = [];
 const messages = [];
+let userMsg;
+
+const getUser = (name) => {
+    let count = -1;
+    for(let i = 0; i < users.length; i++){
+        if(users[i]['name'] === name) count = i;
+        }
+    return count;
+}
 
 const server = net.createServer((socket) => {
     socket.on('data', (data) => {
-        let message = JSON.parse(data);
-        console.log('new connection')
-        if(!users.includes(socket)){
-            users.push(socket);
+        let userData = JSON.parse(data);
+        let userMsg = `${userData['name'] || 'new user'} : ${userData['message']}`;
+        messages.push(userMsg);
+        let history = messages.join('\n');
+        if(userData['type'] === 'setName'){
+            users.push({
+                name: userData['message'],
+                socket: socket
+            });
+            socket.write(JSON.stringify(history));
+        } else if(userData['type'] === 'message'){
+            let userInd = getUser(userData['name']);
+            users[userInd]['message'] = userData['message'];
         }
         for(let user of users){
-            user.write(JSON.stringify(messages.join('\n')))
+            if(getUser(userData['name']) != -1) user.socket.write(JSON.stringify(history));
         }
-        messages.push(message);
+        
     });
+    
+});
+
+server.on('connection', (data) => {
+    console.log('New connection')
 });
 
 server.listen(2000);
